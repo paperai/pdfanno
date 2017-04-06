9d1ea7e8e0f6bf38f6ca2f81e34ac8cfcd2cbb2d
Yohei Munesada <y.munesada@gmail.com>
2017-04-06 13:42:56 +0900

    Public APIs

---
+++ src/page/public.js
@@ -0,0 +1,126 @@
+import { convertFromExportY } from '../shared/coords';
+
+/**
+ * Add an annotation, and render it.
+ */
+export function addAnnotation(publicAnnotation) {
+
+    let a = publicAnnotation.annotation;
+    window.iframeWindow.annotationContainer.add(a);
+    a.render();
+    a.save();
+
+}
+
+/**
+ * Delete an annotation, and also detach it from view.
+ */
+export function deleteAnnotation(publicAnnotation) {
+
+    publicAnnotation.annotation.destroy();
+}
+
+/**
+ * Rect Annotation Class wrapping the core.
+ */
+export class PublicRectAnnotation {
+
+    /**
+     * Create a rect annotation from a TOML data.
+     */
+    constructor(page, position, label='', id=0) {
+
+        // Check inputs.
+        if (!page || typeof page !== 'number') {
+            throw 'Set the page as number.';
+        }
+        if (!position || position.length !== 4) {
+            throw 'Set the position which includes `x`, `y`, `width` and `height`.';
+        }
+
+        let rect = iframeWindow.PDFAnnoCore.RectAnnotation.newInstance({
+            uuid     : id,
+            x        : position[0],
+            y        : convertFromExportY(page, position[1]),
+            width    : position[2],
+            height   : position[3],
+            text     : label,
+            color    : "#FF0000",  // TODO 固定で良い？
+            readOnly : false       // TODO 固定で良い？
+        });
+
+        this.annotation = rect;
+    }
+
+}
+
+/**
+ * Rect Annotation Class wrapping the core.
+ */
+export class PublicSpanAnnotation {
+
+    constructor(page, positions, label='', text='', id=0) {
+
+        // Check inputs.
+        if (!page || typeof page !== 'number') {
+            throw 'Set the page as number.';
+        }
+        if (!positions) {
+            throw 'Set the positions.';
+        }
+
+        // Convert.
+        positions = positions.map(p => {
+            return {
+                page   : page,
+                x      : p[0],
+                y      : convertFromExportY(page, p[1]),
+                width  : p[2],
+                height : p[3]
+            }
+        });
+
+        let span = window.iframeWindow.PDFAnnoCore.SpanAnnotation.newInstance({
+            uuid         : id,
+            rectangles   : positions,
+            text         : label,
+            color        : '#FFFF00',  // TODO 固定で良い？
+            readOnly     : false,      // TODO 固定で良い？
+            selectedText : text
+        });
+
+        this.annotation = span;
+    }
+
+
+}
+
+/**
+ * Rect Annotation Class wrapping the core.
+ */
+export class PublicRelationAnnotation {
+
+    constructor(dir, ids, label='') {
+
+        // Check inputs.
+        if (!dir) {
+            throw 'Set the dir.';
+        }
+        if (!ids || ids.length !== 2) {
+            throw 'Set the ids.';
+        }
+
+        let r = iframeWindow.PDFAnnoCore.RelationAnnotation.newInstance({
+            direction : dir,
+            rel1      : ids[0],
+            rel2      : ids[1],
+            text      : label,
+            color     : "#FF0000",  // TODO 固定で良い？
+            readOnly  : false       // TODO 固定で良い？
+        });
+
+        this.annotation = r;
+    }
+
+
+}
--- src/pdfanno.js
+++ src/pdfanno.js
@@ -22,6 +22,17 @@ import {
     resizeHandler
 } from './page/util/window';
 
+import * as publicApi from './page/public';
+
+/**
+ * Expose public APIs.
+ */
+window.add = publicApi.addAnnotation;
+window.delete = publicApi.deleteAnnotation;
+window.RectAnnotation = publicApi.PublicRectAnnotation;
+window.SpanAnnotation = publicApi.PublicSpanAnnotation;
+window.RelationAnnotation = publicApi.PublicRelationAnnotation;
+
 /**
  * The data which is loaded via `Browse` button.
  */
