79196a61f136585764510283a7ee6bde698994fe
Yohei Munesada <y.munesada@gmail.com>
2017-07-15 01:07:02 +0900

    Set a custom user-agent.

--- server/server2.js
+++ server/server2.js
@@ -45,6 +45,9 @@ app.get('/load_pdf', (req, res) => {
     const reqConfig = {
         method   : 'GET',
         url      : pdfURL,
+        headers : {
+            'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.19 Safari/537.36'
+        },
         encoding : null     // treat a response as a binary.
     };
 
