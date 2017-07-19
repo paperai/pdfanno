1dde59e2fa837be2e854da807f2a3024e3e4115d
Yohei Munesada <y.munesada@gmail.com>
2017-07-19 20:23:45 +0900

    Add comment about local development settings.

--- server/server.js
+++ server/server.js
@@ -4,19 +4,6 @@ const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// Load environment.
-const NODE_ENV = process.env.NODE_ENV;
-const isProduction = NODE_ENV === 'production';
-const isDevelopment = NODE_ENV === 'develop';
-console.log('isProduction=', isProduction);
-console.log('isDevelopment=', isDevelopment);
-
-// Set the static root directory.
-let STATIC_ROOT = '../docs';
-if (!isProduction) {
-    STATIC_ROOT = '../dist';
-}
-
 // create Application.
 const app = express();
 
@@ -24,21 +11,21 @@ const app = express();
 app.use(bodyParser.json({ limit : '50mb' }));
 app.use(bodyParser.urlencoded({ limit : '50mb', expented : true }));
 
-// Setting for static files.
+
+//============ Settings for local development (from) ==================//
+// Routing for static files.
+let STATIC_ROOT = path.join('..', (process.env.NODE_ENV === 'production' ? 'docs' : 'dist'));
 app.use('/dist', express.static(path.resolve(__dirname, STATIC_ROOT, 'dist')));
 app.use('/pages', express.static(path.resolve(__dirname, STATIC_ROOT, 'pages')));
 app.use('/build', express.static(path.resolve(__dirname, STATIC_ROOT, 'build')));
 app.use('/pdfs', express.static(path.resolve(__dirname, STATIC_ROOT, 'pdfs')));
 app.use('/pdfanno.core.bundle.js', express.static(path.resolve(__dirname, STATIC_ROOT, 'pdfanno.core.bundle.js')));
 app.use('/pdfanno.page.bundle.js', express.static(path.resolve(__dirname, STATIC_ROOT, 'pdfanno.page.bundle.js')));
-
-// Rooting : Index file.
-if (isDevelopment) {
-    app.get('/', function(req, res) {
-        res.type('html');
-        res.send(fs.readFileSync(path.resolve(__dirname, STATIC_ROOT, 'index.html')));
-    });
-}
+app.get('/', function(req, res) {
+    res.type('html');
+    res.send(fs.readFileSync(path.resolve(__dirname, STATIC_ROOT, 'index.html')));
+});
+//============ Settings for local development (to) ==================//
 
 
 // Rooting(API) : Uploading a pdf.
@@ -89,10 +76,8 @@ app.get('/load_pdf', (req, res) => {
 });
 
 // Port.
-let port = 1000;
-if (isDevelopment) {
-    port = 3000;
-}
+let port = process.env.NODE_PORT || 1000;
+console.log('PORT:', port);
 
 // Launch app.
 app.listen(port, function() {
