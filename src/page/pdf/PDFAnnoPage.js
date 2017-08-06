f879d21ade0e355ea38d7cbfb50f265b439b3f18
Hiroyuki Shindo <hshindo@outlook.com>
2017-08-06 21:03:21 +0900

    Better error message

--- src/page/pdf/PDFAnnoPage.js
+++ src/page/pdf/PDFAnnoPage.js
@@ -240,7 +240,7 @@ export default class PDFAnnoPage {
 
         // Check empty.
         if (!rects) {
-            return annoUI.ui.alertDialog.show({ message : 'Please select a text span first.' });
+            return annoUI.ui.alertDialog.show({ message : 'Text span is not selected.' });
         }
 
         // Check duplicated.
@@ -298,7 +298,7 @@ export default class PDFAnnoPage {
         });
 
         if (selectedAnnotations.length < 2) {
-            return annoUI.ui.alertDialog.show({ message : 'Please select two annotations first.' });
+            return annoUI.ui.alertDialog.show({ message : 'Two annotated text spans are not selected.\nTo select multiple annotated spans, click the first annotated span, then Ctrl+Click (Windows) or Cmd+Click (OSX) the second span.' });
         }
 
         const first  = selectedAnnotations[selectedAnnotations.length - 2];
