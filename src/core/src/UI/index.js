872b63b034d0cb8823853e76bc2d58a8198c4ad8
Yohei Munesada <y.munesada@gmail.com>
2017-08-28 23:20:15 +0900

    eslint対応

--- src/core/src/UI/index.js
+++ src/core/src/UI/index.js
@@ -1,11 +1,13 @@
-import { disableRect, enableRect } from './rect';
-import { createSpan, getRectangles } from './span';
-import { createRelation } from './relation';
-import { enableViewMode } from './view';
+import { disableRect, enableRect } from './rect'
+import { createSpan, getRectangles } from './span'
+import { createRelation } from './relation'
+import { enableViewMode } from './view'
 
 export default {
-  disableRect, enableRect,
-  createSpan, getRectangles,
-  createRelation,
-  enableViewMode
-};
+    disableRect,
+    enableRect,
+    createSpan,
+    getRectangles,
+    createRelation,
+    enableViewMode
+}
--- src/core/src/utils/tomlString.js
+++ src/core/src/utils/tomlString.js
@@ -1,54 +1,59 @@
+/**
+ * Create a TOML String from jsObject.
+ */
+export default function tomlString (obj, root = true) {
 
-export default function tomlString(obj, root=true) {
-
-    let lines = [];
+    let lines = []
 
     // `version` is first.
     if ('version' in obj) {
-        lines.push(`version = "${obj['version']}"`);
-        lines.push('');
-        delete obj['version'];
+        lines.push(`version = "${obj['version']}"`)
+        lines.push('')
+        delete obj['version']
     }
 
     // #paperanno-ja/issues/38
     // Make all values in `position` as string.
     if ('position' in obj) {
-        let position = obj.position;
+        let position = obj.position
         position = position.map(p => {
             if (typeof p === 'number') {
-                return String(p);
+                return String(p)
             } else {
-                return p.map(v => String(v));
+                return p.map(v => String(v))
             }
-        });
-        obj.position = position;
+        })
+        obj.position = position
     }
 
     Object.keys(obj).forEach(prop => {
 
-        let val = obj[prop];
+        let val = obj[prop]
         if (typeof val === 'string') {
-            lines.push(`${prop} = "${val}"`);
-            root && lines.push('');
+            lines.push(`${prop} = "${val}"`)
+            root && lines.push('')
 
         } else if (typeof val === 'number') {
-            lines.push(`${prop} = ${val}`);
-            root && lines.push('');
+            lines.push(`${prop} = ${val}`)
+            root && lines.push('')
 
         } else if (isArray(val)) {
-            lines.push(`${prop} = ${JSON.stringify(val)}`);
-            root && lines.push('');
+            lines.push(`${prop} = ${JSON.stringify(val)}`)
+            root && lines.push('')
 
         } else if (typeof val === 'object') {
-            lines.push(`[${prop}]`);
-            lines.push(tomlString(val, false));
-            root && lines.push('');
+            lines.push(`[${prop}]`)
+            lines.push(tomlString(val, false))
+            root && lines.push('')
         }
-    });
+    })
 
-    return lines.join('\n');
+    return lines.join('\n')
 }
 
-function isArray(val) {
-    return val && 'length' in val;
+/**
+ * Check the val is array.
+ */
+function isArray (val) {
+    return val && 'length' in val
 }
