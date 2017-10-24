65b3d28dc8a5d40e61c2fdc13818eda5118cdce3
Yohei Munesada <y.munesada@gmail.com>
2017-10-24 23:22:38 +0900

    Add stable build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var fs = require('fs-extra');
 var path = require('path');
+const version = require('./package.json').version;
 
 gulp.task('prepare', function() {
     fs.removeSync('dist');
@@ -9,7 +10,14 @@ gulp.task('prepare', function() {
     fs.copySync('pdfs', path.join('dist', 'pdfs'));
 });
 
-gulp.task('publish', function() {
-    fs.removeSync('docs');
-    fs.copySync('dist', 'docs');
+gulp.task('publish_latest', function() {
+    const baseDir = path.join('docs', 'latest');
+    fs.removeSync(baseDir);
+    fs.copySync('dist', baseDir);
+});
+
+gulp.task('publish_stable', function() {
+    const baseDir = path.join('docs', version);
+    fs.removeSync(baseDir);
+    fs.copySync('dist', baseDir);
 });
