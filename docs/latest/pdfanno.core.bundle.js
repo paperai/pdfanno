734ff3220c55aec6a069a52ad28bd93da17ac811
Yohei Munesada <y.munesada@gmail.com>
2017-12-23 00:22:30 +0900

    Publish

--- docs/latest/pdfanno.core.bundle.js
+++ docs/latest/pdfanno.core.bundle.js
@@ -7512,6 +7512,8 @@ function renderText (a, svg) {
  */
 function renderRelation (a) {
 
+    a.color = a.color || '#F00'
+
     // Adjust the start/end points.
     let theta = Math.atan((a.y1 - a.y2) / (a.x1 - a.x2))
     let sign = (a.x1 < a.x2 ? 1 : -1)
