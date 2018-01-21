8d1e913745ae47ae86cddf7968ca5f20ee77f1f3
Yohei Munesada <y.munesada@gmail.com>
2018-01-21 13:15:22 +0900

    Add an external api for getting an user annotation.

--- server/server.js
+++ server/server.js
@@ -23,14 +23,23 @@ require('./controller/ws')(server)
 app.use(bodyParser.json({ limit : '50mb' }));
 app.use(bodyParser.urlencoded({ limit : '50mb', expented : true }));
 
+/***********************
+    Internal APIs.
+************************/
 // API: upload a pdf and analyze it.
 app.post('/api/pdf_upload', upload.fields([]), controller.internal.uploadPDF);
-
 // API: load a pdf from web.
 app.get('/load_pdf', controller.internal.loadPDF);
-
 // API: load a annotations from web.
 app.get('/api/load_anno', controller.internal.loadAnno);
+
+/***********************
+    External APIs.
+************************/
+app.get('/papi/documents/:documentId/annotations', controller.external.getUserAnnotation)
+
+
+
 
 // Launch the app.
 const port = process.env.NODE_PORT || 1000
