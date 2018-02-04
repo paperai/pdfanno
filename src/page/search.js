86cb2b1eab6851e47914d0134fddf73506d3858b
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2018-02-04 23:47:50 +0900

    [WIP] changed to separate UI and pdfanno specific processes

--- src/page/search.js
+++ src/page/search.js
@@ -3,21 +3,7 @@
  */
 import { paddingBetweenPages, nextZIndex } from '../shared/coords'
 import { customizeAnalyzeResult, extractMeta } from './util/analyzer'
-
-/**
- * The analyze data per pages.
- */
-let pages = []
-
-/**
- * Search type ( text / dictionary )
- */
-let searchType = null
-
-/**
- * The position where a search result is highlighted.
- */
-let searchPosition = -1
+import { searchUI } from 'anno-ui'
 
 /**
  * The highlights for search.
@@ -25,127 +11,38 @@ let searchPosition = -1
 let searchHighlights = []
 
 /**
- * Texts for dictionary search.
- */
-let dictonaryTexts
-
-/**
  * Setup the search function.
  */
 export function setup (analyzeData) {
-
-    pages = customizeAnalyzeResult(analyzeData)
-
-    enableSearchUI()
+    searchUI.setup({
+        pages    : customizeAnalyzeResult(analyzeData),
+        scrollTo    : highlightSearchResult.bind(this), // TODO: 引数としてスクロールすべきヒット文字列位置を得る
+        renderer    : renderHighlight.bind(this),       // TODO: 引数としてpositions(#search() の結果)を受け取る
+        resetUI    : resetUI.bind(this)                 // TODO: ハイライトレンダリングに関するリセットを行う。操作系についてはsearchEngind内にて実施
+    })
+    searchUI.enableSearchUI()
 }
 
 /**
  * Get the current highlight.
  */
 export function getSearchHighlight () {
+    const searchPosition = searchUI.searchPosition()
     if (searchPosition > -1) {
         return searchHighlights[searchPosition]
     }
     return null
 }
 
-function enableSearchUI () {
-    $('#searchWord, .js-dict-match-file').removeAttr('disabled')
-}
-
 window.addEventListener('DOMContentLoaded', () => {
-
-    const DELAY = 500
-    let timerId
-
-    $('#searchWord').on('keyup', e => {
-
-        // Enter key.
-        if (e.keyCode === 13) {
-            nextResult()
-            return
-        }
-
-        if (timerId) {
-            clearTimeout(timerId)
-            timerId = null
-        }
-
-        timerId = setTimeout(() => {
-            timerId = null
-            searchType = 'text'
-            doSearch()
-        }, DELAY)
-    })
-
-    $('.js-search-case-sensitive, .js-search-regexp').on('change', () => {
-        searchType = 'text'
-        doSearch()
-    })
-
-    $('.js-search-case-sensitive, .js-search-regexp').on('click', e => {
-        $(e.currentTarget).blur()
-    })
-
-    $('.js-search-clear').on('click', e => {
-        // Clear search.
-        $('#searchWord').val('')
-        searchType = null
-        doSearch()
-        $(e.currentTarget).blur()
-    })
-
-    // Re-render the search results.
-    window.addEventListener('pagerendered', rerenderSearchResults)
-
-    $('.js-search-prev, .js-search-next').on('click', e => {
-
-        if (searchType !== 'text') {
-            return
-        }
-
-        // No action for no results.
-        if (searchHighlights.length === 0) {
-            return
-        }
-
-        if ($(e.currentTarget).hasClass('js-search-prev')) {
-            prevResult()
-        } else {
-            nextResult()
-        }
-    })
+  // Re-render the search results.
+  window.addEventListener('pagerendered', rerenderSearchResults)
 })
-
-/**
- * Highlight the prev search result.
- */
-function prevResult () {
-    searchPosition--
-    if (searchPosition < 0) {
-        searchPosition = searchHighlights.length - 1
-    }
-    highlightSearchResult()
-}
-
-/**
- * Highlight the next search result.
- */
-function nextResult () {
-    searchPosition++
-    if (searchPosition >= searchHighlights.length) {
-        searchPosition = 0
-    }
-    highlightSearchResult()
-}
 
 /**
  * Highlight a single search result.
  */
-function highlightSearchResult () {
-
-    $('.search-current-position').text(searchPosition + 1)
-
+function highlightSearchResult (searchPosition) {
     $('.pdfanno-search-result', window.iframeWindow.document).removeClass('pdfanno-search-result--highlight')
 
     const highlight = searchHighlights[searchPosition]
@@ -162,9 +59,11 @@ function highlightSearchResult () {
 
 /**
  * Render search results.
+ * TODO: 機能は違いますがrenderHighlightと名前が被ってしまってしまっているのでどちらかを変更
  */
 function rerenderSearchResults () {
 
+    // TODO: これは #resetUI() を呼び出してもよいかも
     // Remove olds.
     $('.pdfanno-search-result', window.iframeWindow.document).remove()
 
@@ -179,120 +78,55 @@ function rerenderSearchResults () {
 }
 
 /**
- * Search the position of  a word / words which an user input.
- */
-function search ({ hay, needle, isCaseSensitive = false, useRegexp = false }) {
-    if (!needle) {
-        return []
-    }
-    const SPECIAL_CHARS_REGEX = /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g
-    const flags = 'g' + (isCaseSensitive === false ? 'i' : '')
-    if (useRegexp === false) {
-        needle = needle.replace(SPECIAL_CHARS_REGEX, '\\$&')
-    }
-    let re = new RegExp(needle, flags)
-    let positions = []
-    let match
-    while ((match = re.exec(hay)) != null) {
-        positions.push({
-            start : match.index,
-            end   : match.index + match[0].length
-        })
-    }
-    return positions
-}
-
-/**
  * Reset the UI display.
+ * @see anno-ui.searchUI#resetUI()
  */
 function resetUI () {
     $('.pdfanno-search-result', window.iframeWindow.document).remove()
-    $('.search-hit').addClass('hidden')
-    $('.js-dict-match-cur-pos, .js-dict-match-hit-counts').text('000')
 }
 
 /**
- * Search the word and display.
+ * render search results as highlight.
  */
-function doSearch ({ query = null } = {}) {
-
-    // Check enable.
-    if ($('#searchWord').is('[disabled]')) {
-        console.log('Search function is not enabled yet.')
-        return
-    }
-
-    resetUI()
-
-    let text
-    let isCaseSensitive
-    let useRegexp
-    if (searchType === 'text') {
-        text = $('#searchWord').val()
-        isCaseSensitive = $('.js-search-case-sensitive')[0].checked
-        useRegexp = $('.js-search-regexp')[0].checked
-    } else {
-        text = query
-        isCaseSensitive = $('.js-dict-match-case-sensitive')[0].checked
-        useRegexp = true
-    }
-
-    console.log(`doSearch: searchType=${searchType} text="${text}", caseSensitive=${isCaseSensitive}, regexp=${useRegexp}`)
-
-    // Reset.
-    searchPosition = -1
-    searchHighlights = []
-
-    // The min length of text for searching.
-    const MIN_LEN = 2
-    if (!text || text.length < MIN_LEN) {
-        return
-    }
-
-    pages.forEach(page => {
-
-        // Search.
-        const positions = search({ hay : page.body, needle : text, isCaseSensitive, useRegexp })
-
-        // Display highlights.
-        if (positions.length > 0) {
-            positions.forEach(position => {
-                const $textLayer = $(`.page[data-page-number="${page.page}"] .textLayer`, window.iframeWindow.document)
-                const infos = page.meta.slice(position.start, position.end)
-                let fromX, toX, fromY, toY
-                infos.forEach(info => {
-                    if (!info) {
-                        return
-                    }
-                    const { x, y, w, h } = extractMeta(info)
-                    fromX = (fromX === undefined ? x : Math.min(x, fromX))
-                    toX = (toX === undefined ? (x + w) : Math.max((x + w), toX))
-                    fromY = (fromY === undefined ? y : Math.min(y, fromY))
-                    toY = (toY === undefined ? (y + h) : Math.max((y + h), toY))
-                })
-                const scale = window.iframeWindow.PDFView.pdfViewer.getPageView(0).viewport.scale
-                let $div = $('<div class="pdfanno-search-result"/>')
-                $div.css({
-                    top    : fromY * scale + 'px',
-                    left   : fromX * scale + 'px',
-                    width  : (toX - fromX) * scale + 'px',
-                    height : (toY - fromY) * scale + 'px',
-                    zIndex : nextZIndex()
-                })
-                $textLayer.append($div)
-                // TODO 後で、改行されたものとかにも対応できるようにする（その場合は、rectsが複数）
-                const aPosition = [[ fromX, fromY, (toX - fromX), (toY - fromY) ]]
-                searchHighlights.push({
-                    page           : page.page,
-                    top            : fromY,
-                    position       : aPosition,
-                    searchPosition : position,
-                    $elm           : $div,
-                    text
-                })
+function renderHighlight(positions, pages) {
+    // Display highlights.
+    if (positions.length > 0) {
+        positions.forEach(position => {
+            const $textLayer = $(`.page[data-page-number="${page.page}"] .textLayer`, window.iframeWindow.document)
+            const infos = page.meta.slice(position.start, position.end)
+            let fromX, toX, fromY, toY
+            infos.forEach(info => {
+                if (!info) {
+                    return
+                }
+                const { x, y, w, h } = extractMeta(info)
+                fromX = (fromX === undefined ? x : Math.min(x, fromX))
+                toX = (toX === undefined ? (x + w) : Math.max((x + w), toX))
+                fromY = (fromY === undefined ? y : Math.min(y, fromY))
+                toY = (toY === undefined ? (y + h) : Math.max((y + h), toY))
             })
-        }
-    })
+            const scale = window.iframeWindow.PDFView.pdfViewer.getPageView(0).viewport.scale
+            let $div = $('<div class="pdfanno-search-result"/>')
+            $div.css({
+                top    : fromY * scale + 'px',
+                left   : fromX * scale + 'px',
+                width  : (toX - fromX) * scale + 'px',
+                height : (toY - fromY) * scale + 'px',
+                zIndex : nextZIndex()
+            })
+            $textLayer.append($div)
+            // TODO 後で、改行されたものとかにも対応できるようにする（その場合は、rectsが複数）
+            const aPosition = [[ fromX, fromY, (toX - fromX), (toY - fromY) ]]
+            searchHighlights.push({
+                page           : page.page,
+                top            : fromY,
+                position       : aPosition,
+                searchPosition : position,
+                $elm           : $div,
+                text
+            })
+        })
+    }
 
     if (searchHighlights.length > 0) {
         // Init highlight at the current page.
@@ -300,114 +134,14 @@ function doSearch ({ query = null } = {}) {
         let found = false
         for (let i = 0; i < searchHighlights.length; i++) {
             if (currentPage === searchHighlights[i].page) {
-                searchPosition = i
+                searchUI.setSearchPosition(i)
                 found = true
                 break
             }
         }
         // If there is no result at the current page, set the index 0.
         if (!found) {
-            searchPosition = 0
+            searchUI.setSearchPosition(0)
         }
-        highlightSearchResult()
     }
-
-    if (searchType === 'text') {
-        $('.search-hit').removeClass('hidden')
-        $('.search-current-position').text(searchPosition + 1)
-        $('.search-hit-count').text(searchHighlights.length)
-    } else {
-        // Dict matching.
-        $('.js-dict-match-cur-pos').text(searchPosition + 1)
-        $('.js-dict-match-hit-counts').text(searchHighlights.length)
-    }
-}
-
-/**
- * Dictonary Matching.
- */
-window.addEventListener('DOMContentLoaded', () => {
-
-    // Clear prev cache.
-    $('.js-dict-match-file :file').on('click', e => {
-        $(e.currentTarget).val(null)
-    })
-
-    // Load a dictionary for matching.
-    $('.js-dict-match-file :file').on('change', e => {
-
-        const files = e.target.files
-        if (files.length === 0) {
-            window.annoUI.ui.alertDialog.show({ message : 'Select a file.' })
-            return
-        }
-
-        const fname = files[0].name
-        $('.js-dict-match-file-name').text(fname)
-
-        let fileReader = new FileReader()
-        fileReader.onload = ev => {
-            const texts = ev.target.result.split('\n').map(t => {
-                return t.trim()
-            }).filter(t => {
-                return t
-            })
-            if (texts.length === 0) {
-                window.annoUI.ui.alertDialog.show({ message : 'No text is found in the dictionary file.' })
-                return
-            }
-            dictonaryTexts = texts
-            searchByDictionary(texts)
-        }
-        fileReader.readAsText(files[0])
-    })
-
-    // Clear search results.
-    $('.js-dict-match-clear').on('click', e => {
-        searchType = null
-        doSearch()
-        $(e.currentTarget).blur()
-    })
-
-    // Go to the prev/next result.
-    $('.js-dict-match-prev, .js-dict-match-next').on('click', e => {
-
-        if (searchType !== 'dictionary') {
-            return
-        }
-
-        // No action for no results.
-        if (searchHighlights.length === 0) {
-            return
-        }
-
-        // go to next or prev.
-        let num = 1
-        if ($(e.currentTarget).hasClass('js-dict-match-prev')) {
-            num = -1
-        }
-        searchPosition += num
-        if (searchPosition < 0) {
-            searchPosition = searchHighlights.length - 1
-        } else if (searchPosition >= searchHighlights.length) {
-            searchPosition = 0
-        }
-
-        highlightSearchResult()
-    })
-
-    // Set the search behavior.
-    $('.js-dict-match-case-sensitive').on('change', () => {
-        searchByDictionary(dictonaryTexts)
-    })
-})
-
-/**
- * Search by a dict file.
- */
-function searchByDictionary (texts = []) {
-    console.log('searchByDictionary:', texts)
-    searchType = 'dictionary'
-    const query = texts.join('|')
-    doSearch({ query })
 }
