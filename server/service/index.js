3cd73f408ed46889243333bbeb062bbf0a851d21
Yohei Munesada <y.munesada@gmail.com>
2018-03-19 23:38:35 +0900

    Attempt to change the request.

--- server/service/index.js
+++ server/service/index.js
@@ -18,7 +18,13 @@ module.exports.fetchPDF = async url => {
         url      : url,
         headers  : {
             // behave as a browser.
-            'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.19 Safari/537.36'
+            'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.124 Safari/537.36',
+            'Upgrade-Insecure-Requests' : '1',
+            'Host' : 'arxiv.org',
+            'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding' : 'gzip, deflate, br',
+            'Accept-Language' : 'ja,en-US;q=0.9,en;q=0.8',
+
         },
         // treat a response as a binary.
         encoding : null
