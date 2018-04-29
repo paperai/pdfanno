f773f18d71cb7a4f6270dd6a22e27c9fa0a1e020
Yohei Munesada <y.munesada@gmail.com>
2018-04-29 23:28:48 +0900

    Refactoring.

--- server/server.js
+++ server/server.js
@@ -1,38 +1,38 @@
-const multer = require('multer');
-const upload = multer();
-const express = require('express');
-const bodyParser = require('body-parser');
-const controller = require('./controller');
+const multer = require('multer')
+const upload = multer()
+const express = require('express')
+const bodyParser = require('body-parser')
+const controller = require('./controller')
 
 // Create an application.
 const app = express()
 const server = require('http').Server(app)
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    next()
+})
 
-// Setup websocket.
+// Setup WebSocket.
 require('./controller/ws')(server)
 
 // Setup the body parser.
-app.use(bodyParser.json({ limit : '50mb' }));
-app.use(bodyParser.urlencoded({ limit : '50mb', expented : true }));
+app.use(bodyParser.json({ limit : '50mb' }))
+app.use(bodyParser.urlencoded({ limit : '50mb', expented : true }))
 
 
 /***********************
     Internal APIs.
 ************************/
 // Upload a PDF and analyze it.
-app.post('/internal/api/pdfs/:documentId', upload.fields([]), controller.internal.uploadPDF);
+app.post('/internal/api/pdfs/:documentId', upload.fields([]), controller.internal.uploadPDF)
 // Load a PDF from the web.
-app.get('/internal/api/pdfs', controller.internal.loadPDF);
+app.get('/internal/api/pdfs', controller.internal.loadPDF)
 // Load an annotation from the web.
-app.get('/internal/api/annotations', controller.internal.loadAnno);
+app.get('/internal/api/annotations', controller.internal.loadAnno)
 
 
 /***********************
@@ -46,5 +46,5 @@ app.get('/api/documents/:documentId/annotations', controller.external.getUserAnn
 // Launch the app.
 const port = process.env.NODE_PORT || 1000
 server.listen(port, function() {
-    console.log(`App listening on port ${port}.`);
-});
+    console.log(`App listening on port ${port}.`)
+})
