a452d669a9d49fabb2e49a0908c375ab2cf80138
Toshiyuki Yamamoto <kikipeco22@gmail.com>
2018-10-13 00:23:40 +0900

    rounding the layer coordinates

--- src/core/src/annotation/layer.js
+++ src/core/src/annotation/layer.js
@@ -1,20 +1,23 @@
 import * as Constants from '../../../shared/constants'
 import * as Utils from '../../../shared/util'
 
+let borderTop = 9
+let merginBetweenPages =  1 + 1
+
 /**
  * Add annotation layer.
  * @param {Integer} page
  */
 export function addAnnoLayer (page = null) {
 
-  console.log('addAnnoLayer', page, window.PDFView.pdfViewer.pagesCount)
+  // console.log('addAnnoLayer', page, window.PDFView.pdfViewer.pagesCount)
 
   if (page === null) {
     for (page = 1; page <= window.PDFView.pdfViewer.pagesCount; page++) {
       addAnnoLayer(page)
     }
   } else {
-    console.log('addAnnoLayer: page=', page)
+    // console.log('addAnnoLayer: page=', page)
 
     const view = window.PDFView.pdfViewer.getPageView(page - 1)
     const $container = Utils.getContainer(page)
@@ -23,15 +26,13 @@ export function addAnnoLayer (page = null) {
     if (view && $container.length > 0 && $annoLayer.length === 0) {
 
       let $annoLayer = $('<div>').addClass(Constants.ANNO_LAYER_CLASS_NAME).css({
-        width  : `${view.width}px`,
-        height : `${view.height}px`
+        width  : `${parseInt(view.width, 10)}px`,
+        height : `${parseInt(view.height, 10) + borderTop + merginBetweenPages}px`
       })
 
       // console.log('before', Utils.getAnnoLayer(page))
-
       $container.append($annoLayer)
-
-      console.log('after', Utils.getAnnoLayer(page))
+      // console.log('after', Utils.getAnnoLayer(page))
     }
   }
 }
